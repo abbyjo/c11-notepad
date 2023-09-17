4b931596dbe9629d90b420ba1370426d8ed1057b
abbyjo <abbyjo225@gmail.com>
2023-09-17 16:12:37 -0400

    Added structure for API routes

--- server.js
+++ server.js
@@ -2,15 +2,16 @@
 const express = require('express');
 const path = require('path');
 
-//Declarations
+// Declarations
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 
-//HTML routes
-// GET
+// HTML routes
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 })
@@ -18,13 +19,21 @@ app.get('/notes', (req, res) => {
 app.get('*', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
-// POST
 
-//API routes
+// API routes
+// GET
+app.get('/api/notes', (req, res) => {
 
-// GET 
+})
 // POST 
+app.post('/api/notes', (req, res) => {
+
+})
 // DELETE 
+app.delete('/api/notes/:id', (req, res) => {
+
+})
+
 
 app.listen(PORT, () =>
   console.log(`Live and listening at port ${PORT}★`)
