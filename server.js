5f5a59862ae5f7d4847f2340c006620b51d69b3f
abbyjo <abbyjo225@gmail.com>
2023-09-17 13:56:53 -0400

    Added server js file with basics

---
+++ server.js
@@ -0,0 +1,21 @@
+// Imported modules
+const express = require('express');
+const path = require('path');
+
+//Declarations
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.static('public'));
+
+
+//HTML routes
+
+// GET
+// POST
+
+//API routes
+
+// GET 
+// POST 
+// DELETE 
