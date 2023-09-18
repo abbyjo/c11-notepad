c64d1c5e2fe5e99733197afe00fe611c8c6f9f57
abbyjo <abbyjo225@gmail.com>
2023-09-17 22:25:32 -0400

    Finalized API post route

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const storedNotes = require('./db/db.json')
+const storedNotes = require('./db/db.json');
+const { randomUUID } = require('crypto');
+
 // Declarations
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -12,38 +14,40 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // API routes
-// GET
+
+//GET - Returns stored notes as JSON 
 app.get('/api/notes', (req, res) => {
     res.json(storedNotes)
 })
-// POST 
+
+//POST - Adds and saves new note to database 
 app.post('/api/notes', (req, res) => {
-    if ( req.body ) {
-        storedNotes.push(req.body)
+    const {title, text } = req.body;
+    if ( title && text ) {
+        const myNote = {
+            title,
+            text,
+            id: randomUUID()
+        };
+        storedNotes.push(myNote)
+        fs.writeFile('./db/db.json', JSON.stringify(storedNotes), (err) => {
+                err ? console.error(err) : console.log('New note has been added!')
+            })
 
-        //NOTE TO SELF: Need to make new note permanant in DB....append not working in below method
-        // perhaps its an issue w/the data payload?? it seems "push" and "appendFile" aren't compatible
-
-        // function saveMyNote (note) {
-        //     storedNotes.push(note)
-        // }
-        // myNote = JSON.stringify(saveMyNote(req.body))
-        // fs.appendFile('./db/db.json',myNote, (err) =>{
-        //     err ? console.error(err) : console.log('New note has been added!')
-        // });
-        
         console.log('Request recieved successfully!')
-        res.status(200).json({status: "Note saved successfully~"}) //lies.... FOR NOW 
+        res.status(200).json({status: "Note saved successfully~"})
     } else {
         res.status(500).json('Note unable to be saved :-( Try adding a title or some text!')
     }
 })
+
 // DELETE 
 app.delete('/api/notes/:id', (req, res) => {
 
 })
 
-// HTML routes
+
+// HTML routes - Return relevant HTML pages to client
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 })
